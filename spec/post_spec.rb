4ca19c1a2097fa4a69bb7a95dba0701741e05cd6
Azhar Shah <azharshahstl@yahoo.com>
2019-10-15 19:04:21 +0000

    Automatically backed up by Learn

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Post do 
   it 'can be created' do 
-    post = Post.create! 
+    post = Post.create!(title: "My title", description: "The post description")
+    expect (post).to
   
 end
